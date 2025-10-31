f49ac4ca49f6aaab4130f37a6485ab9322563f86
cbentsfeeco-prog <cbents.feeco@gmail.com>
2025-10-31 06:25:26 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
 
         // Render the Turnstile widget
         (window as any).turnstile.render(turnstileRef.current, {
-          sitekey: '0x4AAAAAABaeDHdwHVmJobfE',
+          sitekey: '0x4AAAAAAB99rE2Np99oPpTc',
           theme: 'dark',
           callback: (token: string) => {
             // Redirect on successful verification
